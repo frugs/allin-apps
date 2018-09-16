26565cfc1241ed002348e94aae0c88218242af4a
Hugo Wainwright <hugowainwright@fb.com>
2018-09-16 23:11:05 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from zergmacroplot import app as zerg_macro_app
+import werkzeug.wsgi
+import werkzeug.serving
+import werkzeug.wrappers
+
+app = werkzeug.wsgi.DispatcherMiddleware(
+    werkzeug.wrappers.Response("Nothing to see here!", status=404),
+    mounts={
+        "/zergmacro": zerg_macro_app
+    })
+
+
+def main():
+    werkzeug.serving.run_simple(
+        "127.0.0.1", 37696, app, use_reloader=True, use_debugger=True)
+
+
+if __name__ == "__main__":
+    main()
