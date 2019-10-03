9106ed208a304de59bca6bdb629b77de796be679
Hugo Wainwright <hugowainwright@fb.com>
2019-10-04 00:25:29 +0100

    added allin practice

---
+++ allin-apps-1/allin-practice/precache-manifest.c43725f9cf5f4b97bd21bc0f1cd69fc7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b107988e9132da6bfb7d5948341d34",
+    "url": "//all-inspiration-apps.appspot.com/practice/index.html"
+  },
+  {
+    "revision": "90bba7cb7b2b87a35c1e",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/css/2.caec78ab.chunk.css"
+  },
+  {
+    "revision": "bd80c5e89cedf1d0ba06",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/css/main.e6c9d9b6.chunk.css"
+  },
+  {
+    "revision": "90bba7cb7b2b87a35c1e",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/js/2.954147f0.chunk.js"
+  },
+  {
+    "revision": "bd80c5e89cedf1d0ba06",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/js/main.01f24c9e.chunk.js"
+  },
+  {
+    "revision": "a5975d4b21085b43f357",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/js/runtime-main.20e58c7a.js"
+  },
+  {
+    "revision": "88f72fcea489649ddb12f3d765dc43ad",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/allin_cover.88f72fce.png"
+  },
+  {
+    "revision": "22ac2fdd071ebc98869d25cdbbbb5bcc",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/allin_logo.22ac2fdd.png"
+  },
+  {
+    "revision": "8731c59d30c11341fe9ca34a3a1690e5",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/bronze_icon.8731c59d.png"
+  },
+  {
+    "revision": "7b57604cd9b36c0f9513e009a1d8f152",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/diamond_icon.7b57604c.png"
+  },
+  {
+    "revision": "843e6c09e8e8052681fdf5da7a1df321",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/ethnocentric_rg.843e6c09.ttf"
+  },
+  {
+    "revision": "c5e72b8420d91526b69906ab1f4ea5e4",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/gold_icon.c5e72b84.png"
+  },
+  {
+    "revision": "6e5ed5fb502cf55b7cb4c657a0694689",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/grandmaster_icon.6e5ed5fb.png"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/icons-20.90247cfd.ttf"
+  },
+  {
+    "revision": "a346bae0d74cdf963525debb493bc919",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/master_icon.a346bae0.png"
+  },
+  {
+    "revision": "edfb887b96148c11561a1879fac8cb0c",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/platinum_icon.edfb887b.png"
+  },
+  {
+    "revision": "7a6832e792dab765022f3e5851ce0ba5",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/protoss_icon.7a6832e7.png"
+  },
+  {
+    "revision": "0f76d118a8038610c9179e25b0fd9ff4",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/random_icon.0f76d118.png"
+  },
+  {
+    "revision": "8aada9413f27708a49e1fd50d8735205",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/silver_icon.8aada941.png"
+  },
+  {
+    "revision": "0a91dd89f3eadb1a32fcc51d257ad61a",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/terran_icon.0a91dd89.png"
+  },
+  {
+    "revision": "ecf1d83cdaeb7bcadb35be8eb36fd2f9",
+    "url": "//all-inspiration-apps.appspot.com/practice/static/media/zerg_icon.ecf1d83c.png"
+  }
+]);
